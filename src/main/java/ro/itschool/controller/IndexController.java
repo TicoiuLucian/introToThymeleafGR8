71302fd8f1bd85c5caa4d22e3fb6e064bb32cc45
Ticoiu Lucian <lucian.ticoiu.ext@alten.com>
2023-02-28 18:40:02 +0200

    add "/" to @RequestMapping

--- src/main/java/ro/itschool/controller/IndexController.java
+++ src/main/java/ro/itschool/controller/IndexController.java
@@ -4,12 +4,14 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import ro.itschool.entity.Car;
 import ro.itschool.repository.CarRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-@RequestMapping("/index")
+@RequestMapping({"/index", "/"})
 @RequiredArgsConstructor
 public class IndexController {
 
@@ -19,7 +21,8 @@ public class IndexController {
     //-----------------Display all cars-------------------
     @RequestMapping
     public String getIndex(Model model) {
-        model.addAttribute("cars", carRepository.findAll());
+        List<Car> allCars = carRepository.findAll();
+        model.addAttribute("cars", allCars);
         return "index";
     }
     //----------------------------------------------------
