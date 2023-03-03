ac7ff5aa4470625047e0f316c55747d398932d7b
Ticoiu Lucian <lucian.ticoiu.ext@alten.com>
2023-03-03 10:39:18 +0200

    add data to data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,15 @@
 -- insert 10 available cars
- insert into car (add_date, manufacturer, model, price, sold, sold_date) values (curdate(), 'Audi', 'A7', 19500, 0, NULL)
+ insert into car (add_date, manufacturer, model, price, sold, sold_date) values (curdate(), 'Audi', 'A7', 35500, 0, NULL)
+ insert into car (add_date, manufacturer, model, price, sold, sold_date) values (curdate(), 'BMW', 'X3', 17800, 0, NULL)
+ insert into car (add_date, manufacturer, model, price, sold, sold_date) values (curdate(), 'Volkswagen', 'Passat', 9600, 0, NULL)
+ insert into car (add_date, manufacturer, model, price, sold, sold_date) values (curdate(), 'Mercedes', 'C200', 13400, 0, NULL)
+ insert into car (add_date, manufacturer, model, price, sold, sold_date) values (curdate(), 'Nissan', 'Pathfinder', 8750, 0, NULL)
+ insert into car (add_date, manufacturer, model, price, sold, sold_date) values (curdate(), 'Renault', 'Megane', 10630, 0, NULL)
+ insert into car (add_date, manufacturer, model, price, sold, sold_date) values (curdate(), 'Mitsubishi', 'Lancer', 22530, 0, NULL)
+ insert into car (add_date, manufacturer, model, price, sold, sold_date) values (curdate(), 'Renault', 'Arkana', 32100, 0, NULL)
+ insert into car (add_date, manufacturer, model, price, sold, sold_date) values (curdate(), 'Volkswagen', 'Touareg', 18900, 0, NULL)
+ insert into car (add_date, manufacturer, model, price, sold, sold_date) values (curdate(), 'Audi', 'A3', 19500, 0, NULL)
 
---insert 5 sold cars
+--insert 2 sold cars
+ insert into car (add_date, manufacturer, model, price, sold, sold_date) values (curdate(), 'Volkswagen', 'Touareg', 19750, 1, '2020-10-05')
+ insert into car (add_date, manufacturer, model, price, sold, sold_date) values (curdate(), 'Volvo', 'XC60', 22300, 1, '2022-10-10')
