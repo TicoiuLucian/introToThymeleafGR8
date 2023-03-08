9bee61ad813d157ce135c915c2e49b288a9ee747
PaulaRoxana <paula_girdea@yahoo.com>
2023-03-08 19:28:10 +0200

    person - commit

--- src/main/java/ro/itschool/repository/PersonRepository.java
+++ src/main/java/ro/itschool/repository/PersonRepository.java
@@ -1,4 +1,11 @@
 package ro.itschool.repository;
 
-public interface PersonRepository {
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import ro.itschool.entity.Person;
+
+public interface PersonRepository extends JpaRepository<Person, Integer> {
+
+
+  List<Person> findByNameAndAddress(String name, String address);
 }
